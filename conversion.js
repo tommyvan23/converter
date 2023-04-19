ae9da7c42821895810ea573383f46aff38660c72
tommyvan23 <26727644+tommyvan23@users.noreply.github.com>
2023-04-19 16:00:03 -0600

    Update conversion.js

--- conversion.js
+++ conversion.js
@@ -13,7 +13,6 @@ function placeFileContent(target, file) {
       target.value = content;
     }).catch(error => console.log(error))
   }
-
 function readFileContent(file) {
 	const reader = new FileReader()
   return new Promise((resolve, reject) => {
@@ -26,13 +25,17 @@ document.getElementById('inputFile')
 .addEventListener('change', getFile)
 document.getElementById ("function").addEventListener ("click", convertFile, false);
 function convertFile(){
-   
+
 //Replacements
 fileString = fileString.replace(/\*/g, "=");
+fileString = fileString.split('a.'||'a)'||'A)'||'(A)'|| '(a)'&& !(('a.\r') || ('a. \r') || ('a.\n') || ('a. \n'))).join('~%0%a)');
+fileString = fileString.split('b.'||'b)'||'B)'||'(B)' || '(b)'&& !(('b.\r') || ('b. \r') || ('b.\n') || ('b. \n'))).join('~%0%b)');
+fileString = fileString.split('c.'||'c)'||'C)'||'(C)' || '(c)'&& !(('c.\r') || ('c. \r') || ('c.\n') || ('c. \n'))).join('~%0%c)');
+fileString = fileString.split('d. ' || 'd.'||'d)'||'D)'||'(D)' || '(d)' && !(('d.\r') || ('d. \r') || ('d.\n') || ('d. \n'))).join('~%0%d)');
 fileString = fileString.split('a.'||'a)'||'A)'||'(A)'|| '(a)'&& !(('a.\r') || ('a. \r') || ('a.\n') || ('a. \n'))&& !(('a\r') || ('a \r') || ('a\n') || ('a \n'))).join('~%0%a)');
 fileString = fileString.split('b.'||'b)'||'B)'||'(B)' || '(b)'&& !(('b.\r') || ('b. \r') || ('b.\n') || ('b. \n'))&& !(('b\r') || ('b \r') || ('b\n') || ('b \n'))).join('~%0%b)');
 fileString = fileString.split('c.'||'c)'||'C)'||'(C)' || '(c)'&& !(('c.\r') || ('c. \r') || ('c.\n') || ('c. \n'))&& !(('c\r') || ('c \r') || ('c\n') || ('c \n'))).join('~%0%c)');
-fileString = fileString.split('d.'||'d)'||'D)'||'(D)' || '(d)' && !(('d.\r') || ('d. \r') || ('d.\n') || ('d. \n'))&& !(('d\r') || ('d \r') || ('d\n') || ('d \n'))).join('~%0%d)');
+fileString = fileString.split('d. ' || 'd.'||'d)'||'D)'||'(D)' || '(d)' && !(('d.\r') || ('d. \r') || ('d.\n') || ('d. \n'))&& !(('d\r') || ('d \r') || ('d\n') || ('d \n'))).join('~%0%d)');
 fileString = fileString.split(/True/ || /true/).join('~%0%True');
 fileString = fileString.split((/~%0%True(?=\s.)/) || (/=%100%True(?=\s.)/)).join('True');
 fileString = fileString.split(/False/ || /false/).join('~%0%False');
@@ -50,30 +53,20 @@ fileString = fileString.replace(/\b\~c\)/g,'c')
 fileString = fileString.replace(/\b\~b\)/g,'b')
 fileString = fileString.replace(/\b\~a\)/g,'a')
 fileString = fileString.replace(/\n\s*\n/g, '\n');
-
 // Split into lines
 let splitLines = fileString.split(/\r?\n/);
-
-
 // Returns correct format
   splitLines = splitLines.map(function(element)
   {if (element.startsWith ('::::[choice_multiple]<p>')) 
     {element.replace(/<p> /g, '<p>'); 
         return (element + '</p>' + '{')
-    } 
-  } else {return element};
+    }  else {return element};
     });
     for (let i = 0; i < splitLines.length; i++) {
       if (i < splitLines.length - 1 && splitLines[i + 1].startsWith(' //') || ((i === splitLines.length - 1))) {
         splitLines[i] += '\n } \n';
-      } 
-    });
-for (let j = 0; j < splitLines.length; j++) {
-  let line = splitLines[j];
-  if (line.endsWith(".") || line.endsWith(" ") || line.endsWith("\r") || line.endsWith("\n")) {
-    splitLines[j] = line.slice(0, -1);
-  }
-});
+      }
+    }
   splitLines.shift();
   console.log(splitLines);
   splitLines = splitLines.join('\r\n');
@@ -81,15 +74,12 @@ for (let j = 0; j < splitLines.length; j++) {
 console.log(splitLines)
 //Sends to Output
 const blob = new Blob([splitLines], {type: 'text/plain'});
-
 // Create a download link for the blob object
 const downloadLink = document.createElement('a');
 downloadLink.href = URL.createObjectURL(blob);
 downloadLink.download = 'output.txt';
-
 // Append the download link to the body
 document.body.appendChild(downloadLink);
-
 // Click the download link to start the download
 downloadLink.click();
 };
